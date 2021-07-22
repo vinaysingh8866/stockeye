7d91c037a6508f918333079da79e541201718aed
Nirja <nirjapathak3@gmail.com>
2021-07-21 20:34:51 -0700

    Login UI Page

--- ui/LoginPage.js
+++ ui/LoginPage.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput,
+    Button, TouchableOpacity} from 'react-native';
 
 export class LoginPage extends Component {
+     
     render() {
+       
         return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
+            <View style={styles.container}>
+   
+        <StatusBar style="auto" />
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email."
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+          />
+        </View>
+   
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password."
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+   
+        <TouchableOpacity>
+          <Text style={styles.forgot_button}>Forgot Password?</Text>
+        </TouchableOpacity>
+   
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
         )
     }
 }
