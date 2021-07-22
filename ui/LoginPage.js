70b7978018cc8396b4af460991eea158f15dba59
Nirja <nirjapathak3@gmail.com>
2021-07-21 20:41:23 -0700

    Changes in LogIn page

--- ui/LoginPage.js
+++ ui/LoginPage.js
@@ -35,7 +35,11 @@ export class LoginPage extends Component {
         </TouchableOpacity>
    
         <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
+          <Text style={styles.loginText}>SIGNIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.signupBtn}>
+          <Text style={styles.loginText}>SIGNUP</Text>
         </TouchableOpacity>
       </View>
         )
@@ -43,3 +47,58 @@ export class LoginPage extends Component {
 }
 
 export default LoginPage
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+   
+    image: {
+      marginBottom: 40,
+    },
+   
+    inputView: {
+      backgroundColor: "#FFC0CB",
+      borderRadius: 30,
+      width: "70%",
+      height: 45,
+      marginBottom: 20,
+   
+      alignItems: "center",
+    },
+   
+    TextInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    },
+   
+    forgot_button: {
+      height: 30,
+      marginBottom: 30,
+    },
+   
+    loginBtn: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#FF1493",
+    },
+
+    signupBtn: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        backgroundColor: "#FF1493",
+      },
+  });
+
