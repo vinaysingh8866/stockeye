1aa616f48ce444e6100875997ba2c9728116d361
Vinay Singh <vinaysingh8866@gmail.com>
2021-08-10 19:12:01 -0400

    Buy Sell Section Added For Strategy

--- ui/StrategyPage.js
+++ ui/StrategyPage.js
@@ -1,8 +1,52 @@
 import React, { Component, useState } from 'react'
-import { Text, View, SafeAreaView, StyleSheet, Picker, Button, TouchableOpacity, Modal, TextInput, Alert } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet, Picker, Button, TouchableOpacity, Modal, TextInput, Alert, FlatList} from 'react-native'
 import ModalAdd from '../components/ModalAdd';
 
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'RSI',
+      CC:70,
+      DD:'>'
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'BBP',
+      CC:1,
+      DD:'>'
+    },
+    
+  ];
+  
 
+  const DATAB = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'RSI',
+      CC:30,
+      DD:'<'
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'BBP',
+      CC:0,
+      DD:'<'
+    },
+    
+  ];
+  const renderItem = ({ item }) => (
+  
+  <TouchableOpacity>
+    <View style={{width:'80%',height:60,backgroundColor:'#F2F0F0', marginBottom:10,marginTop:20,borderRadius:16,shadowColor:'black',shadowRadius:1,shadowOffset:{width:3, height:3},shadowOpacity:.1}}>
+      <View style={{flexDirection:'row',flexWrap: "wrap",width:'100%',height:'100%',backgroundColor:'#F2F0F0',borderRadius:16, shadowColor:'white',shadowRadius:1,shadowOffset:{width:-3, height:-3},shadowOpacity:3}}>
+        <Text style={{ fontSize:15, marginLeft:'10%',marginTop:20}}>{item.title}</Text>
+        <Text style={{ alignSelf: "flex-start", fontSize:15, marginLeft:'30%',marginTop:20}}>{item.DD}{item.CC}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+  
+  //<TileSmall name={item.title}/>
+  );
 
 
 export class StrategyPage extends Component {
@@ -23,7 +67,6 @@ export class StrategyPage extends Component {
         
         return (
             <SafeAreaView>
-
                 <View style={styles.container}>
                     <View style={{ 
                                 marginStart:20,
@@ -62,10 +105,66 @@ export class StrategyPage extends Component {
                             </View>
                         </View>
                     <View>
-                        <ModalAdd/>
+                        <ModalAdd />
                     </View>
                 </View>
-                <Text>OKOK{this.state.text}</Text>
+                <View style={{flexDirection:'row', marginTop:20}}>
+                    <Text style={{width:'50%', textAlign:'center', fontSize:27}}>Buy</Text>
+                    <Text style={{width:'50%', textAlign:'center', fontSize:27}}>Sell</Text>
+                </View>
+                <View style={{flexDirection:'row'}}>
+                    <FlatList
+                    style={{marginLeft:'5%',paddingStart:10}}
+                    //horizontal
+                    //horizontal={true}
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    showsHorizontalScrollIndicator={false}
+                    />
+                    <FlatList
+                    style={{marginLeft:'5%',paddingStart:10}}
+                    //horizontal
+                    //horizontal={true}
+                    data={DATAB}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    showsHorizontalScrollIndicator={false}
+                    />
+                </View>
+                <TouchableOpacity>
+                    <View style={{ 
+                                marginStart:'30%',
+                                marginTop:20,
+                                width:150,
+                                height:70,
+                                borderRadius:16,
+                                shadowColor:'black',
+                                shadowRadius:1,
+                                shadowOffset:{width:3, height:3},
+                                shadowOpacity:.1}
+
+                                }>
+
+                            <View style={{ 
+                                backgroundColor: 'pink', 
+                                //marginLeft:'5%', 
+                                width:'100%', 
+                                height:'100%', 
+                                borderRadius:16,
+                                shadowColor:'white',
+                                shadowRadius:1,
+                                shadowOffset:{width:-3, height:-3},
+                                shadowOpacity:3,
+                                alignItems:'center'
+                                
+                                }}> 
+                                
+                                <Text style={{paddingTop:'15%'}}>Test Strategy</Text>
+
+                            </View>
+                    </View>
+                </TouchableOpacity>
                 
             
             </SafeAreaView>
